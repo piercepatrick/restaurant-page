239a4b77cdeebba8c38bf87d985f266465c95d2b
piercepatrick <piercepatrick2001@gmail.com>
2021-04-29 11:11:32 -0500

    Add display functions and listeners to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import { LoadContents } from './home.js'
+import { loadContactContent, deleteContactContent } from './contact.js'
 
+
+let displaying = 'none'
 LoadContents()
+
+const contactRadio = document.getElementById('tab-1')
+contactRadio.addEventListener("click", displayContact)
+
+const menuRadio = document.getElementById('tab-2')
+menuRadio.addEventListener('click', displayMenu)
+
+function displayContact() {
+        if (displaying == 'none') {
+            loadContactContent()
+        }
+        else if (displaying == 'menu') {
+            deleteMenuContent()
+            loadContactContent()
+        }
+        displaying = 'contact'
+}
+
+function displayMenu() {
+    if (displaying == 'none') {
+        loadMenuContent()
+    }
+    else if (displaying == 'contact') {
+        deleteContactContent
+        loadMenuContent()
+    }
+    displaying = 'menu'
+}
